3923d0b2b9e8874ea53ced189d1a758236441714
407-34-phattanakan <54800@yupparaj.ac.th>
2026-08-21 11:25:32 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -100,5 +100,5 @@ if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2)
 
 st.divider()
-st.write("นางสาวพัฒนการร์ คำทอน เลขที่ 34 ม.4/7")
+st.write("นางสาวพัฒนการณ์ คำทอน เลขที่ 34 ม.4/7")
 
