6c21d0543b74547c13ac37eb76c7551bf36dae6d
407-34-phattanakan <54800@yupparaj.ac.th>
2026-08-21 11:51:52 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -127,3 +127,6 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
     show_result_dialog(ans1, ans2, ans3, ans4)
+
+st.divider()
+st.write("นางสาวพัฒนการณ์ คำทอน เลขที่ 34 ม.4/7")
